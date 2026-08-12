9814f474cd73f217c595799f2a23c7ae4f692d43
Paulo Junior <paulonp@proton.me>
2026-08-12 14:01:35 -0300

    fix ctrl d glitch again since my last commit didnt send it for some reason

--- src/cmds/clear.rs
+++ src/cmds/clear.rs
@@ -1,5 +1,5 @@
 pub fn run(_args: &[&str]) {
-    print!("{}[2J", 27 as char);
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     println!("╔══════════════════════╗");
     println!("║     OS Emulator      ║");
     println!("╚══════════════════════╝");
